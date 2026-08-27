9d9c9ab9b5a7709e57c26ba3f1134e83544f3add
Julian41541 <128689035+Julian41541@users.noreply.github.com>
2026-08-27 17:31:18 +0300

    chore: add src/ConstrainCert.Core/CertificateConstants.cs

---
+++ src/ConstrainCert.Core/CertificateConstants.cs
@@ -0,0 +1,8 @@
+namespace ConstrainCert.Core;
+
+public static class CertificateConstants
+{
+    public const string OriginalRootSha256 = "D26D2D0231B7C39F92CC738512BA54103519E4405D68B5BD703E9788CA8ECF31";
+    public const string AnchorCommonName = "ConstrainCert Local Constrained MinDigital Anchor";
+    public const string StateVersion = "1";
+}
